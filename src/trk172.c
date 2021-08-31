1d554b7da9da934a40fb1d7b97f71cead0dc642b
sven4500 <sven4500@mail.ru>
2021-09-01 00:36:59 +0300

    Добавлена функция формирующая команду получения статуса от ТРК.

---
+++ src/trk172.c
@@ -0,0 +1,71 @@
+#include <string.h> // memset
+#include <errno.h>
+#include <trk172.h>
+
+// Не все ОС дают определение для EOK, определяем сами.
+#ifndef EOK
+#define EOK 0
+#endif
+
+uint8_t calc_xor(void const* buf, int const size)
+{
+	uint8_t const* const addr = (uint8_t*)buf;
+	uint8_t sum = 0;
+
+	for (int i = 0; i < size; ++i)
+	{
+		sum ^= addr[i];
+	}
+
+	return sum;
+}
+
+int to_string(char* buf, int max_len, int n)
+{
+	if (n < 0)
+	{
+		return EINVAL;
+	}
+
+	if (max_len == 1)
+	{
+		buf[0] = '0' + n % 10;
+		return EOK;
+	}
+	else
+	if (max_len == 2)
+	{
+		buf[0] = '0' + n / 10;
+		buf[1] = '0' + n % 10;
+		return EOK;
+	}
+	else
+	{
+		return EINVAL;
+	}
+}
+
+int trk172_state(uint8_t* buf, int max_size, int trk_no)
+{
+	if (max_size < TRK172_MSG_LEN)
+	{
+		return ENOMEM;
+	}
+
+	if (trk_no > 0x7F)
+	{
+		return EINVAL;
+	}
+
+	TRK172_MSG* const msg = (TRK172_MSG*)buf;
+	memset(buf, 0, TRK172_MSG_LEN);
+
+	to_string(msg->trk_no, sizeof(msg->trk_no), trk_no);
+	msg->soh = 1;
+	msg->stx = 2;
+	msg->etx = 3;
+	msg->command[0] = TRK172_COM_STATE;
+	msg->crc = calc_xor(buf + 1, TRK172_MSG_LEN - 2);
+
+	return EOK;
+}
---
+++ src/trk172.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef TRK172_H_
+#define TRK172_H_
+
+#include <stdint.h> // uint8_t
+
+#define TRK172_MSG_LEN sizeof(TRK172_MSG)
+
+#define TRK172_COM_STATE ('\x34')
+#define TRK172_COM_LOAD ('\x31')
+#define TRK172_COM_START ('\x35')
+#define TRK172_COM_STOP ('\x36')
+#define TRK172_COM_RESET ('\x37')
+
+typedef struct
+{
+	uint8_t soh;
+	char trk_no[2];
+	char command[1];
+	uint8_t stx;
+	char price[6];
+	char volume[6];
+	char error[2];
+	char code[2];
+	uint8_t etx;
+	uint8_t crc;
+
+} TRK172_MSG;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int trk172_state(uint8_t* buf, int max_size, int trk_no);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
